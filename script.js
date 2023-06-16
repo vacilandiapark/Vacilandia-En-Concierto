98cd166e799e0ebb8ec9fe5b5fde2f3b21fd67fb
Vacilandia Park <michaeld@vacilandia.com.pe>
2023-06-15 22:40:00 -0500

    update

--- script.js
+++ script.js
@@ -99,3 +99,36 @@ particlesJS("particles-js", {
     },
     "retina_detect": true
 });
+
+$(document).ready(function () {
+    // Agrega la clase 'animate-zoom' para la animación inicial
+    $('#carouselExampleControls img').addClass('animate-zoom');
+
+    // Agrega la clase 'image-fade-in' cuando la imagen esté en el viewport
+    $(window).scroll(function () {
+        $('.animate-zoom').each(function () {
+            var imagePos = $(this).offset().top;
+            var topOfWindow = $(window).scrollTop();
+
+            if (imagePos < topOfWindow + $(window).height() - 200) {
+                $(this).addClass('image-fade-in fade-in');
+            }
+        });
+    });
+});
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+      var windowHeight = $(window).height();
+      var scrollTop = $(window).scrollTop();
+  
+      $(".card").each(function() {
+        var cardTop = $(this).offset().top;
+  
+        if (cardTop < windowHeight + scrollTop) {
+          $(this).addClass("fade-in");
+        }
+      });
+    });
+  });
+  
