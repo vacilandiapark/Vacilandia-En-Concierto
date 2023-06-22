27af35b0ca8ff4ec7e7f772c77bab60e95a4a3e6
Michael <michaeld@vacilandia.com.pe>
2023-06-22 12:57:09 -0500

    update

--- script.js
+++ script.js
@@ -1,181 +1,157 @@
+window.addEventListener('scroll', function () {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+// Script para controlar el carrusel de imágenes
+// Script para controlar el carrusel de imágenes
+const carrusel = document.querySelector('.carrusel');
+const imagenes = document.querySelectorAll('.carrusel .imagen');
+const botonAnterior = document.querySelector('.carrusel .boton.anterior');
+const botonSiguiente = document.querySelector('.carrusel .boton.siguiente');
+let index = 0;
+
+// Función para mostrar la imagen siguiente
+function carruselSiguiente() {
+    index = (index + 1) % imagenes.length;
+    actualizarCarrusel();
+}
+
+// Función para mostrar la imagen anterior
+function carruselAnterior() {
+    index = (index - 1 + imagenes.length) % imagenes.length;
+    actualizarCarrusel();
+}
+
+// Función para actualizar el carrusel con la imagen actual
+function actualizarCarrusel() {
+    imagenes.forEach((imagen, i) => {
+        imagen.style.transform = `translateX(-${index * 100}%)`;
+        imagen.style.opacity = i === index ? 1 : 0;
+    });
+}
+
+// Función para mostrar la primera imagen al cargar la página
+function mostrarPrimeraImagen() {
+    imagenes[index].style.opacity = 1;
+}
+
+// Asignar eventos a los botones del carrusel
+botonAnterior.addEventListener('click', carruselAnterior);
+botonSiguiente.addEventListener('click', carruselSiguiente);
+
+// Mostrar la primera imagen al cargar la página
+mostrarPrimeraImagen();
+
+// Cambiar automáticamente cada 2 segundos
+setInterval(carruselSiguiente, 2000);
+
+
+// PARTICULAS //
+
 particlesJS("particles-js", {
-    "particles": {
-        "number": {
-            "value": 80,
-            "density": {
-                "enable": true,
-                "value_area": 800
+    particles: {
+        number: {
+            value: 100,
+            density: {
+                enable: true,
+                value_area: 800
             }
         },
-        "color": {
-            "value": "#ffffff"
+        color: {
+            value: "#ffffff"
         },
-        "shape": {
-            "type": "circle",
-            "stroke": {
-                "width": 0,
-                "color": "#ffffff"
+        shape: {
+            type: "circle",
+            stroke: {
+                width: 0,
+                color: "#000000"
             },
-            "polygon": {
-                "nb_sides": 5
+            polygon: {
+                nb_sides: 5
             },
-            "image": {
-                "src": "img/github.svg",
-                "width": 100,
-                "height": 100
+        },
+        opacity: {
+            value: 0.5,
+            random: true,
+            anim: {
+                enable: true,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false
             }
         },
-        "opacity": {
-            "value": 0.5,
-            "random": false,
-            "anim": {
-                "enable": false,
-                "speed": 1,
-                "opacity_min": 0.1,
-                "sync": false
+        size: {
+            value: 1,
+            random: true,
+            anim: {
+                enable: false,
+                speed: 40,
+                size_min: 0.1,
+                sync: false
             }
         },
-        "size": {
-            "value": 1,
-            "random": true,
-            "anim": {
-                "enable": false,
-                "speed": 40,
-                "size_min": 0.1,
-                "sync": false
+        line_linked: {
+            enable: false
+        },
+        move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            random: true,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
             }
-        },
-        "line_linked": {
-            "enable": false
-        },
-        "move": {
-            "enable": true,
-            "speed": 1,
-            "direction": "none",
-            "random": true,
-            "straight": false,
-            "out_mode": "out",
-            "bounce": false
         }
     },
-    "interactivity": {
-        "detect_on": "canvas",
-        "events": {
-            "onhover": {
-                "enable": true,
-                "mode": "grab"
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: {
+                enable: true,
+                mode: "repulse"
             },
-            "onclick": {
-                "enable": true,
-                "mode": "push"
+            onclick: {
+                enable: true,
+                mode: "push"
             },
-            "resize": true
+            resize: true
         },
-        "modes": {
-            "grab": {
-                "distance": 140,
-                "line_linked": {
-                    "opacity": 1
+        modes: {
+            grab: {
+                distance: 400,
+                line_linked: {
+                    opacity: 1
                 }
             },
-            "bubble": {
-                "distance": 400,
-                "size": 40,
-                "duration": 2,
-                "opacity": 8,
-                "speed": 3
+            bubble: {
+                distance: 400,
+                size: 40,
+                duration: 2,
+                opacity: 8,
+                speed: 3
             },
-            "repulse": {
-                "distance": 200,
-                "duration": 0.4
+            repulse: {
+                distance: 200
             },
-            "push": {
-                "particles_nb": 4
+            push: {
+                particles_nb: 4
             },
-            "remove": {
-                "particles_nb": 2
+            remove: {
+                particles_nb: 2
             }
         }
     },
-    "retina_detect": true
+    retina_detect: true
 });
 
-
-// CARRUSEL //
-
-document.addEventListener('DOMContentLoaded', function () {
-    var carouselItems = document.querySelectorAll('.carousel-item');
-    var mousePos = { x: 0, y: 0 };
-
-    carouselItems.forEach(function (item) {
-        item.addEventListener('mousemove', function (event) {
-            var rect = item.getBoundingClientRect();
-            var itemPos = { x: event.clientX - rect.left, y: event.clientY - rect.top };
-            var dx = (itemPos.x - rect.width / 2) / (rect.width / 2) * 0.1;
-            var dy = (itemPos.y - rect.height / 2) / (rect.height / 2) * 0.1;
-            item.style.transform = 'scale(1.1) translate(' + dx + 'px, ' + dy + 'px)';
-        });
-
-        item.addEventListener('mouseleave', function () {
-            item.style.transform = 'scale(1) translate(0, 0)';
-        });
-    });
-
-    function updateItemTransform() {
-        var activeItem = document.querySelector('.carousel-item.active');
-        var rect = activeItem.getBoundingClientRect();
-        var itemPos = { x: mousePos.x - rect.left, y: mousePos.y - rect.top };
-        var dx = (itemPos.x - rect.width / 2) / (rect.width / 2) * 0.1;
-        var dy = (itemPos.y - rect.height / 2) / (rect.height / 2) * 0.1;
-        activeItem.style.transform = 'scale(1.1) translate(' + dx + 'px, ' + dy + 'px)';
-        carouselItems.forEach(function (item) {
-            if (item !== activeItem) {
-                item.style.transform = 'scale(1) translate(0, 0)';
-            }
-        });
-
-        requestAnimationFrame(updateItemTransform);
-    }
-
-    // Añadir un evento al contenedor del carrusel para rastrear la posición del ratón
-    document.querySelector('#carouselExampleControls').addEventListener('mousemove', function (event) {
-        mousePos.x = event.clientX;
-        mousePos.y = event.clientY;
-    });
-
-    // Iniciar el bucle de actualización del carrusel
-    updateItemTransform();
-});
-
-
-// Configuración de ScrollReveal
-ScrollReveal().reveal('.card-item', {
-    delay: 50,                 // Retardo antes de la animación (en milisegundos)
-    duration: 1000,             // Duración de la animación (en milisegundos)
-    origin: 'bottom',           // Desde dónde se originará la animación
-    distance: '50px',           // Distancia de desplazamiento durante la animación
-    opacity: 0,                 // Opacidad inicial del elemento
-    scale: 0.8,                 // Escala inicial del elemento
-    easing: 'cubic-bezier(0.5, 0, 0, 1)',   // Curva de aceleración de la animación
-    reset: true,                // Restablecer animación al desplazarse hacia arriba
-    interval: 200               // Intervalo entre la animación de cada elemento
-});
-
-$(document).ready(function() {
-    // Agrega el desplazamiento suave a todos los enlaces dentro del navbar
-    $(".navbar-nav a").on("click", function(event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-
-            var hash = this.hash;
-
-            // Desplazamiento suave utilizando el método animate de jQuery
-            $("html, body").animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function() {
-                // Agrega el hash (#) a la URL después de desplazarse
-                window.location.hash = hash;
-            });
-        }
-    });
-});
+AOS.init();
